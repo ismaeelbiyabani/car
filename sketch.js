f3756a778c9ba8f03ecf7e2347fe2c3e96bd4e05
ftg-gtf <67550388+ftg-gtf@users.noreply.github.com>
2020-10-04 17:21:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 var car,wall;
-var speed;weight;
+var speed,weight;
 function setup() {
   createCanvas(1000,1000);
   car=createSprite(50,200,50,50)
-  car.shapeColour=colour(0,255,0)
+  car.shapeColour="green";
   speed=random(55,90)
   weight=random(400,500)
  
